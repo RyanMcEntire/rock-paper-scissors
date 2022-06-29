431a1bc9bb731819444e0690450f6c7cfd6f50d3
RyanMcEntire <ryanmcentire89@gmail.com>
2022-06-29 16:37:14 -0600

    add function for player input and convert string to lowercase

--- main.js
+++ main.js
@@ -16,18 +16,35 @@ function computerPlay() {
 const pick = randomNumber();
 
 
-
 function playRound(playerSelection, computerSelection) {
-    if ("rock", "paper") {
-        return "You lose.";
-    }   else if ("rock", "scissors") {
-        return "You win!";
-    }   else if ("rock", "rock") {
-        return "Tie!"
-    }
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Rock loses to Paper. You lose...";
+    }   else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "Rock beats Scissors. You win!";
+    }   else if (playerSelection == "rock" && computerSelection == "rock") {
+        return "Rock matches Rock. Its a tie!";
+    }   else if (playerSelection == "paper" && computerSelection == "paper") {
+        return "Paper matches Paper. Its a tie!";
+    }   else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "Paper Loses to Scissors. You lose...";
+    }   else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "Paper beats Rock. You win!";
+    }   else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "Scissors beats Paper. You win!";
+    }   else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return "Scissors matches Scissors. Its a tie!";
+    }   else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "Scissors loses to Rock. You lose.";
+}
 }
 
-const playerSelection = "rock";
+
+
 const computerSelection = computerPlay();
 
-console.log(playRound);
+const playerSelection = prompt("Choose your weapon: rock, paper, scissors").toLowerCase();
+
+console.log(playRound(playerSelection, computerSelection)); 
+
+
+
