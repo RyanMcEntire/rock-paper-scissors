60c783b9db52c911050070f69a2c680eb6d18b72
RyanMcEntire <ryanmcentire89@gmail.com>
2022-06-29 18:27:01 -0600

    Add full function of the lesson for rps.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function randomNumber() {
     return Math.floor(Math.random() * 3);
-    }
-
+}
 
 function computerPlay() {
+    const pick = randomNumber();
     if (pick == 0) {
         return "rock";
     } else if (pick == 1) {
@@ -13,38 +13,65 @@ function computerPlay() {
     }
 }
 
-const pick = randomNumber();
-
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        return "Rock loses to Paper. You lose...";
-    }   else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "Rock beats Scissors. You win!";
-    }   else if (playerSelection == "rock" && computerSelection == "rock") {
-        return "Rock matches Rock. Its a tie!";
-    }   else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "Paper matches Paper. Its a tie!";
-    }   else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "Paper Loses to Scissors. You lose...";
-    }   else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "Paper beats Rock. You win!";
-    }   else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "Scissors beats Paper. You win!";
-    }   else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "Scissors matches Scissors. Its a tie!";
-    }   else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "Scissors loses to Rock. You lose.";
+function playRound(p, c) {
+    if (p == "rock" && c == "paper") {
+        console.log( "Rock loses to Paper. You lose...") 
+        return -1
+    }   else if (p == "rock" && c == "scissors") {
+        console.log( "Rock beats Scissors. You win!")
+        return 1
+    }   else if (p == "rock" && c == "rock") {
+        console.log( "Rock matches Rock. Its a tie!")
+        return 0
+    }   else if (p == "paper" && c == "paper") {
+        console.log( "Paper matches Paper. Its a tie!")
+        return 0
+    }   else if (p == "paper" && c == "scissors") {
+        console.log( "Paper Loses to Scissors. You lose...")
+        return -1
+    }   else if (p == "paper" && c == "rock") {
+        console.log( "Paper beats Rock. You win!")
+        return 1
+    }   else if (p == "scissors" && c == "paper") {
+        console.log( "Scissors beats Paper. You win!")
+        return 1
+    }   else if (p == "scissors" && c == "scissors") {
+        console.log( "Scissors matches Scissors. Its a tie!")
+        return 0
+    }   else if (p == "scissors" && c == "rock") {
+        console.log( "Scissors loses to Rock. You lose.")
+        return -1
+    }
 }
+
+function game() {
+    let pScore = 0;
+    let cScore = 0;
+    let results = 0;
+    for (let i = 0; i < 5; i++) {
+        const c = computerPlay();
+        let p = prompt("Choose your weapon: rock, paper, scissors");
+        p = p.toLowerCase();
+        const round = playRound(p,c);
+        results = results + round;
+        if (round > 0) {
+            pScore++;
+        }   
+        if (round < 0) {
+            cScore++;
+        }
+    }
+
+    console.log("The match is over!", pScore, cScore);
+    if (results > 0) {
+        console.log("You won!");
+    }   
+    if (results < 0) {
+        console.log("You lost!");
+    }
+    if (results === 0) {
+        console.log("It was a tie. Better luck next time...")
+    }
 }
 
-
-
-const computerSelection = computerPlay();
-
-const playerSelection = prompt("Choose your weapon: rock, paper, scissors").toLowerCase();
-
-console.log(playRound(playerSelection, computerSelection)); 
-
-
-
+game()
