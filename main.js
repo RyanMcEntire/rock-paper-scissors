9f15ca94d0250149896087e15bd8e50cad50c979
RyanMcEntire <ryanmcentire89@gmail.com>
2022-07-05 23:14:26 -0600

    add buttons for player choice

--- main.js
+++ main.js
@@ -1,72 +1,110 @@
 function randomNumber() {
-    return Math.floor(Math.random() * 3);
+  return Math.floor(Math.random() * 3);
 }
 
 function computerPlay() {
-    const pick = randomNumber();
-    if (pick == 0) {
-        return "rock";
-    } else if (pick == 1) {
-        return "paper";
-    } else if (pick == 2) {
-        return "scissors";
-    }
+  const pick = randomNumber();
+  if (pick == 0) {
+    return "rock";
+  } else if (pick == 1) {
+    return "paper";
+  } else if (pick == 2) {
+    return "scissors";
+  }
 }
-    
 
 function playRound(p, c) {
-    if  (p === c) {
-        console.log(`${p.charAt(0).toUpperCase() + p.slice(1)} matches ${c.charAt(0).toUpperCase() + c.slice(1)}. Tie!`);
-        return 0;
-    }    if (p === "rock" && c === "paper") {
-        console.log( "Rock loses to Paper. You lose..."); 
-        return -1;
-    }   if (p === "rock" && c === "scissors") {
-        console.log( "Rock beats Scissors. You win!");
-        return 1;
-    }   if (p === "paper" && c === "scissors") {
-        console.log( "Paper Loses to Scissors. You lose...");
-        return -1;
-    }   if (p === "paper" && c === "rock") {
-        console.log( "Paper beats Rock. You win!");
-        return 1;
-    }   if (p === "scissors" && c === "paper") {
-        console.log( "Scissors beats Paper. You win!");
-        return 1;
-    }   if (p === "scissors" && c === "rock") {
-        console.log( "Scissors loses to Rock. You lose.");
-        return -1;
-    }
+  if (p === c) {
+    console.log(
+      `${p.charAt(0).toUpperCase() + p.slice(1)} matches ${
+        c.charAt(0).toUpperCase() + c.slice(1)
+      }. Tie!`
+    );
+    return 0;
+  }
+  if (p === "rock" && c === "paper") {
+    console.log("Rock loses to Paper. You lose...");
+    return -1;
+  }
+  if (p === "rock" && c === "scissors") {
+    console.log("Rock beats Scissors. You win!");
+    return 1;
+  }
+  if (p === "paper" && c === "scissors") {
+    console.log("Paper Loses to Scissors. You lose...");
+    return -1;
+  }
+  if (p === "paper" && c === "rock") {
+    console.log("Paper beats Rock. You win!");
+    return 1;
+  }
+  if (p === "scissors" && c === "paper") {
+    console.log("Scissors beats Paper. You win!");
+    return 1;
+  }
+  if (p === "scissors" && c === "rock") {
+    console.log("Scissors loses to Rock. You lose.");
+    return -1;
+  }
 }
 
+/*
 function game() {
-    let pScore = 0;
-    let cScore = 0;
-    let results = 0;
-    for (let i = 0; i < 5; i++) {
-        const c = computerPlay();
-        let p = prompt("Choose your weapon: rock, paper, scissors");
-        p = p.toLowerCase();
-        const round = playRound(p,c);
-        results = results + round;
-        if (round === 0) {
-            i--;
-        }
-        if (round > 0) {
-            pScore++;
-        }   
-        if (round < 0) {
-            cScore++;
-        }
+  let pScore = 0;
+  let cScore = 0;
+  let results = 0;
+  for (let i = 0; i < 5; i++) {
+    const c = computerPlay();
+    let p = prompt("Choose your weapon: rock, paper, scissors");
+    p = p.toLowerCase();
+    const round = playRound(p, c);
+    results = results + round;
+    if (round === 0) {
+      i--;
     }
+    if (round > 0) {
+      pScore++;
+    }
+    if (round < 0) {
+      cScore++;
+    }
+  }
 
-    console.log("The match is over!", pScore, cScore);
-    if (results > 0) {
-        console.log("You won!");
-    }   
-    if (results < 0) {
-        console.log("You lost!");
-    }
+  console.log("The match is over!", pScore, cScore);
+  if (results > 0) {
+    console.log("You won!");
+  }
+  if (results < 0) {
+    console.log("You lost!");
+  }
 }
 
-game()
+game();
+*/
+
+const pRock = document.querySelector("#rock");
+pRock.addEventListener("click", () => {
+  const c = computerPlay();
+  let p = pRock.value;
+  playRound(p, c);
+
+  console.log(p, c);
+});
+
+const pPaper = document.querySelector("#paper");
+pPaper.addEventListener("click", () => {
+  const c = computerPlay(); // computer selection
+  let p = pPaper.value; // player selection
+  playRound(p, c);
+
+  console.log(p, c);
+});
+
+const pScissors = document.querySelector("#scissors");
+pScissors.addEventListener("click", () => {
+  const c = computerPlay(); // computer selection
+  let p = pScissors.value; // player selection
+  playRound(p, c);
+
+  console.log(p, c);
+});
