f504c5b7e617a6c26671919b635ed70067ac16ab
RyanMcEntire <ryanmcentire89@gmail.com>
2022-07-06 23:32:28 -0600

    add framework to play 5 rounds and display score

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
+let results = 0;
+let round = 0;
+
+if (round === 5) {
+  
+  console.log(results, playerScore, computerScore);
+}
+
 function randomNumber() {
   return Math.floor(Math.random() * 3);
 }
@@ -13,7 +23,25 @@ function computerPlay() {
   }
 }
 
+const winLoseTie = document.querySelector("#winLoseTie");
+
 function playRound(p, c) {
+  if (round === 5) {
+    pRock.disabled = true;
+    pPaper.disabled = true;
+    pScissors.disabled = true;
+
+    if (playerScore > computerScore) {
+      gameResults.textcontent = `Congrats!, you won the game! Player Score: ${playerScore} Computer Score: ${computerScore}`;
+    }
+    if (computerScore > playerScore) {
+      gameResults.textContent =
+        `Sorry, you lost this one... Player Score: ${playerScore} Computer Score: ${computerScore}`;
+    }
+    
+       
+      console.log(round);
+  }
   if (p === c) {
     console.log(
       `${p.charAt(0).toUpperCase() + p.slice(1)} matches ${
@@ -23,28 +51,40 @@ function playRound(p, c) {
     return 0;
   }
   if (p === "rock" && c === "paper") {
-    console.log("Rock loses to Paper. You lose...");
-    return -1;
+    winLoseTie.textContent = "Rock loses to Paper. You lose...";
+    //console.log("Rock loses to Paper. You lose...");
+    round++;
+    return computerScore++;
   }
   if (p === "rock" && c === "scissors") {
-    console.log("Rock beats Scissors. You win!");
-    return 1;
+    winLoseTie.textContent = "Rock beats Scissors. You win!";
+    //console.log("Rock beats Scissors. You win!");
+    round++;
+    return playerScore++;
   }
   if (p === "paper" && c === "scissors") {
-    console.log("Paper Loses to Scissors. You lose...");
-    return -1;
+    winLoseTie.textContent = "Paper Loses to Scissors. You lose...";
+    //console.log("Paper Loses to Scissors. You lose...");
+    round++;
+    return computerScore++;
   }
   if (p === "paper" && c === "rock") {
-    console.log("Paper beats Rock. You win!");
-    return 1;
+    winLoseTie.textContent = "Paper beats Rock. You win!";
+    //console.log("Paper beats Rock. You win!");
+    round++;
+    return playerScore++;
   }
   if (p === "scissors" && c === "paper") {
-    console.log("Scissors beats Paper. You win!");
-    return 1;
+    winLoseTie.textContent = "Scissors beats Paper. You win!";
+    //console.log("Scissors beats Paper. You win!");
+    round++;
+    return playerScore++;
   }
   if (p === "scissors" && c === "rock") {
-    console.log("Scissors loses to Rock. You lose.");
-    return -1;
+    winLoseTie.textContent = "Scissors loses to Rock. You lose.";
+    //console.log("Scissors loses to Rock. You lose.");
+    round++;
+    return computerScore++;
   }
 }
 
@@ -82,6 +122,7 @@ function game() {
 game();
 */
 
+/*
 const pRock = document.querySelector("#rock");
 pRock.addEventListener("click", () => {
   const c = computerPlay();
@@ -93,8 +134,8 @@ pRock.addEventListener("click", () => {
 
 const pPaper = document.querySelector("#paper");
 pPaper.addEventListener("click", () => {
-  const c = computerPlay(); // computer selection
-  let p = pPaper.value; // player selection
+  const c = computerPlay();
+  let p = pPaper.value;
   playRound(p, c);
 
   console.log(p, c);
@@ -102,8 +143,72 @@ pPaper.addEventListener("click", () => {
 
 const pScissors = document.querySelector("#scissors");
 pScissors.addEventListener("click", () => {
-  const c = computerPlay(); // computer selection
-  let p = pScissors.value; // player selection
+  const c = computerPlay();
+  let p = pScissors.value;
+  playRound(p, c);
+
+  console.log(p, c);
+});
+*/
+/*
+const pSelection = document.querySelectorAll("button");
+
+for (const pPicked of pSelection) {
+  pPicked.addEventlistener("click", (e) => {
+    const c = computerPlay();
+    let p = e.target.class.value;
+    const round = playRound(p, c);
+    console.log(p);
+  });
+};
+*/
+
+/*
+function game() {
+  let pScore = 0;
+  let cScore = 0;
+  let results = 0;
+  for (let i = 0; i < 5; i++) {
+    const pSelection = document.querySelectorAll("button");
+    for (const pChoice of pSelection);
+    {
+      pChoice.addEventListener("click", () => {
+        return pChoice.value;
+      });
+      let p = pChoice.value;
+      const c = computerPlay();
+      playRound(p, c);
+      console.log(p, c);
+    }
+  }
+}
+
+game();
+
+*/
+
+const pRock = document.querySelector("#rock");
+pRock.addEventListener("click", () => {
+  const c = computerPlay();
+  let p = pRock.value;
+  playRound(p, c);
+
+  console.log(p, c);
+});
+
+const pPaper = document.querySelector("#paper");
+pPaper.addEventListener("click", () => {
+  const c = computerPlay();
+  let p = pPaper.value;
+  playRound(p, c);
+
+  console.log(p, c);
+});
+
+const pScissors = document.querySelector("#scissors");
+pScissors.addEventListener("click", () => {
+  const c = computerPlay();
+  let p = pScissors.value;
   playRound(p, c);
 
   console.log(p, c);
